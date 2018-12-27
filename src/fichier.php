97deb9e58efb282d3c7c9b2840b2c296fa86aa3f
s3ij1nn <s3ij1nn@gmail.com>
2018-12-27 15:16:15 +0900

    antivirus scan add

--- src/fichier.php
+++ src/fichier.php
@@ -67,6 +67,15 @@ class fichier
         return $this->request($json, "file/info.cgi");
     }
 
+    public function av_scan($url)
+    {
+        $json = [
+            "url"       =>  $url,
+            "pretty"    =>  1
+        ];
+        return $this->request($json, "file/scan.cgi");
+    }
+
     public function folder_ls($folder_id)
     {
         $json = [
