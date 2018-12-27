85c429ef4a5c5dba19e1c98a9ba0e4e98ffce22f
s3ij1nn <s3ij1nn@gmail.com>
2018-12-27 15:29:49 +0900

    file_cp

--- src/fichier.php
+++ src/fichier.php
@@ -98,6 +98,20 @@ class fichier
         return $this->request($json, "file/mv.cgi");
     }
 
+    public function file_cp($urls, $folder_id)
+    {
+        if(!is_array($urls)){
+            $urls = [$urls];
+        }
+        $json = [
+            "pretty"                    =>  1,
+            "destination_folder_id"     =>  $folder_id,
+            "urls"                      =>  $urls
+        ];
+
+        return $this->request($json, "file/cp.cgi");
+    }
+
     public function folder_ls($folder_id)
     {
         $json = [
